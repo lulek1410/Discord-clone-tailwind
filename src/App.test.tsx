9ca1e50f7a499ca7f2122645b9c25f7147ab0b05
lulek1410 <jan.szewczynski@gmail.com>
2024-10-06 20:40:01 +0200

    Remove redundant files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-describe("App", () => {
-  test("renders navigation", async () => {
-    render(<App/>);
-    expect(screen.getByRole("navigation")).toBeInTheDocument();
-  });
-});
