fbb92edda769f17019b96888057e8e3f91c554fe
lulek1410 <jan.szewczynski@gmail.com>
2024-10-10 19:44:36 +0200

    Fix tests not handling absolute imports

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,34 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const tsconfig = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "./tsconfig.app.json"), "utf-8")
+);
+
 export default {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", {
-      tsconfig: "tsconfig.app.json", 
-    }],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.app.json",
+      },
+    ],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    "^.+\\.svg$": "jest-transformer-svg",
+    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+      prefix: "<rootDir>/",
+    }),
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  transformIgnorePatterns: [
-    "/node_modules/(?!@your-scope/)", 
-  ],
+  transformIgnorePatterns: ["/node_modules/(?!@your-scope/)"],
 };
